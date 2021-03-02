efa7e4fe3fa4c0e4cc483dab7f8ff34c588bfccb
Derrick Moseti <50165747+moseti1@users.noreply.github.com>
2021-03-02 18:02:35 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -2,7 +2,7 @@ from decouple import config
 import dj_database_url
 
 import os
-from local_settings import SECRET_KEY
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
