467683a1a42a5d39bbacec30c8060d48a0672f32
moseti1 <mosetiderrick1@gmail.com>
2021-03-02 20:46:58 +0300

    modified wsgi

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 #application = get_wsgi_application()
-from dj_static import Cling
+
 application = Cling(get_wsgi_application())
