db091a3141aaacc9f051b78bdc7e878ed2c2d8e4
Derrick Moseti <50165747+moseti1@users.noreply.github.com>
2021-03-02 23:36:58 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -104,8 +104,8 @@ if not DEBUG:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.path.join(BASE_DIR, 'db.postgres'),
         }
     }
 
