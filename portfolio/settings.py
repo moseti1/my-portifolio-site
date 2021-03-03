23a00d0844201ee40915edc5c24b493d1087b7db
moseti1 <mosetiderrick1@gmail.com>
2021-03-03 10:09:02 +0300

    modified settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -172,5 +172,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(config=locals(), staticfiles=False,logging=False)
-del DATABASES['default']['OPTIONS']['sslmode']
+# del DATABASES['default']['OPTIONS']['sslmode']
 
